e07c117b5fc48400e8144c19516a9962d0e406b7
Manish Chaturvedi <manish15gkp@gmail.com>
2025-08-05 01:40:49 +0530

    Single loop problem

--- src/Loops/ProblemsOnLoops.java
+++ src/Loops/ProblemsOnLoops.java
@@ -1,6 +1,9 @@
 package Loops;
 
+//import java.math.BigInteger;
+import java.lang.Math;
 import java.util.Scanner;
+
 
 public class ProblemsOnLoops {
     public static void main(String[] args) {
@@ -20,11 +23,53 @@ public class ProblemsOnLoops {
 //        int num  = sc.nextInt();
 //        int sum = 0;
 //        while(num>0){
-//            sum = num % 10 + sum;
+//            sum += num % 10;
 //            num /= 10;
 //        }
-//        System.out.println("The sum of given natural number is : "+sum);
+//        System.out.println("The sum of digit of given natural number is : "+sum);
 
-        //3-->
+        //3-->Reverse the digit of a number
+//        int num = sc.nextInt();
+//        int reverse = 0;
+//        while(num >0){
+//            reverse = (reverse* 10 + num % 10);
+//            num = num /10;
+//        }
+//        System.out.println(reverse);
+
+        //4-->Find the sum of the following series
+        //Doubt arise but clear --first give input number than check on by one according to the rule u write
+//        int num = sc.nextInt();
+//        int total = 0;
+//        for(int i = 1 ; i <= num; i++) {
+//            if (i % 2 == 0) {
+//                total -= i;
+//            } else {
+//                total += i;
+//            }
+//        }
+//        System.out.println(total);
+
+        //5-->Print the first n factorial number
+        //NOTE: for bigInt you should first import than intialize
+        //for multiply use x.multiply(BigInteger.valueOg(y));
+//        long num = sc.nextLong();
+//        BigInteger fact  = BigInteger.ONE;
+//        for( int i = 1 ;  i <= num ; i++){
+//            fact = fact.multiply(BigInteger.valueOf(i));
+//            System.out.println("Factorial of " + i + " : " + fact);
+//        }
+//       System.out.println(fact);
+
+        //6-->Given 2 number a and b. Find a raise to the power b(use pow)
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int ans = 1;
+        for(int i = 1; i <= b; i++ ){
+            ans *= a;
+        }
+        System.out.println(ans);
+        //using of pow method
+        System.out.println(Math.pow(a,b));
     }
 }
