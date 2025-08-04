da5973de45b5cd6825064c8336c4e4bc02d625d8
Manish Chaturvedi <manish15gkp@gmail.com>
2025-08-04 23:29:57 +0530

    some loop questions

--- src/Loops/ProblemsOnLoops.java
+++ src/Loops/ProblemsOnLoops.java
@@ -5,12 +5,26 @@ import java.util.Scanner;
 public class ProblemsOnLoops {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        int count = 0;
-        while(num > 0){
-            num = num / 10;
-            count = count+1;
-        }
-        System.out.println(count);
+
+        //1-->Count the number of digits for a given number n
+//        int num = sc.nextInt();
+//        int count = 0;
+//        while(num > 0){
+//            num = num / 10;
+//            count ++;
+//        }
+//        System.out.println(count);
+
+        //2-->Find the sum of digits in a given number n
+//        System.out.print("Enter a natural number: ");
+//        int num  = sc.nextInt();
+//        int sum = 0;
+//        while(num>0){
+//            sum = num % 10 + sum;
+//            num /= 10;
+//        }
+//        System.out.println("The sum of given natural number is : "+sum);
+
+        //3-->
     }
 }
