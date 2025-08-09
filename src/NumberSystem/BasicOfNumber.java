e7063c8ff7316f1868074d9862eae2e494827262
Manish Chaturvedi <manish15gkp@gmail.com>
2025-08-09 23:44:43 +0530

    number

---
+++ src/NumberSystem/BasicOfNumber.java
@@ -0,0 +1,14 @@
+package NumberSystem;
+
+import java.util.Scanner;
+
+public class BasicOfNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Decimal number have base 10
+        //3225=3*10^3 + 2*10^2 + 2*10^1 + 2*10^0
+
+        //Binary number have base 2
+        //1001=1*2^3 + 0*2^2 + 0*2^1 + 1*2^0
+    }
+}
