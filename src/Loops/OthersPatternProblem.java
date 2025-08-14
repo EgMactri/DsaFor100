aa0991151e39dd636333479401bed05b265e5365
Manish Chaturvedi <manish15gkp@gmail.com>
2025-08-14 22:48:17 +0530

    some patterns

---
+++ src/Loops/OthersPatternProblem.java
@@ -0,0 +1,9 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class OthersPatternProblem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
